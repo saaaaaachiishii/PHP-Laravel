ef81c5f84a4aa1122644599891c81cb63c61733c
sachi ishii <sachii858@gmail.com>
2020-08-08 08:12:41 +0000

    PHP/Laravel 09 Routingについて理解する教材完了

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+/*
+■課題1
+URLとControllerやActionを紐付ける機能を何といいますか？
+→Routing
+
+■課題2
+あなたが考える、group化をすることのメリットを考えてみてください。
+→group化すれば、URlとAction名だけ変えれば良いのでRoutingしやすく、
+ 誰が見た時でも分かりやすいと思います。
+
+■課題3
+Route::get('XXX', 'AAAController@bbb');
+　　　　　　↑URL　　↑Controllerの名前　　↑Actionの名前
+*/
+
+Route::group(['prefix' => 'admin'], function() {
+     Route::get('admin/profile/create', 'ProfileController@add');
+     Route::get('admin/profile/edit', 'ProfileController@edit');
+});
