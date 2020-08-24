c430c8414722dae2ee73b1b621f527804fad67d9
sachi ishii <sachii858@gmail.com>
2020-08-24 11:56:52 +0000

    PHP/Laravel 12 ユーザー認証を実装する 教材完了

--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,13 @@ Route::group(['prefix' => 'admin'], function() {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
